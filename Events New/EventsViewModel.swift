62bd59ac21d8f16df3031d3561322a3032d268a6
Subhadeep Pal <subhadeepal@gmail.com>
2017-06-15 00:35:28 +0530

    Dates Udated

--- Events New/EventsViewModel.swift
+++ Events New/EventsViewModel.swift
@@ -25,11 +25,11 @@ class EventsViewModel: NSObject {
         
         events = [
             Event(name: "For the love of coffee!", address: "The Flying Squirrel", location: "Bengaluru", guestList: guestList, startDate: "2017-07-29 19:30:00", endDate: "2017-07-29 21:30:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: ""),
-            Event(name: "Aero India 2017", address: "The Flying Squirrel", location: "Bengaluru", guestList: guestList, startDate: "2017-06-14 11:00:00", endDate: "2017-06-14 21:30:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: ""),
-            Event(name: "Hot air balloon safari", address: "XYZ Street", location: "Pune", guestList: guestList, startDate: "2017-06-12 11:00:00", endDate: "2017-06-12 13:00:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: "", isSoldOut: true),
-            Event(name: "For the love of coffee!", address: "The Flying Squirrel", location: "Bengaluru", guestList: guestList, startDate: "2017-07-29 19:30:00", endDate: "2017-07-29 21:30:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: ""),
-            Event(name: "Aero India 2017", address: "The Flying Squirrel", location: "Bengaluru", guestList: guestList, startDate: "2017-06-14 11:00:00", endDate: "2017-06-14 21:30:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: ""),
-            Event(name: "Hot air balloon safari", address: "XYZ Street", location: "Pune", guestList: guestList, startDate: "2017-06-12 11:00:00", endDate: "2017-06-12 13:00:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: "", isSoldOut: true),
+            Event(name: "Aero India 2017", address: "The Flying Squirrel", location: "Bengaluru", guestList: guestList, startDate: "2017-06-16 11:00:00", endDate: "2017-06-16 21:30:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: ""),
+            Event(name: "Hot air balloon safari", address: "XYZ Street", location: "Pune", guestList: guestList, startDate: "2017-06-17 11:00:00", endDate: "2017-06-17 13:00:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: "", isSoldOut: true),
+            Event(name: "For the love of coffee!", address: "The Flying Squirrel", location: "Bengaluru", guestList: guestList, startDate: "2017-08-02 19:30:00", endDate: "2017-08-02 21:30:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: ""),
+            Event(name: "Aero India 2017", address: "The Flying Squirrel", location: "Bengaluru", guestList: guestList, startDate: "2017-06-15 11:00:00", endDate: "2017-06-15 21:30:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: ""),
+            Event(name: "Hot air balloon safari", address: "XYZ Street", location: "Pune", guestList: guestList, startDate: "2017-12-12 11:00:00", endDate: "2017-12-12 13:00:00", dressCode: "Smart Casuals", ticketCost: 700, eventImageUrl: "", isSoldOut: true),
             
         ]
         
