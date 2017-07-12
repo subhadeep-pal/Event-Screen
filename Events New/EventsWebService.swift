e592cf7217b408b6ba24eabc545e5e0f958dce67
Subhadeep Pal <subhadeep.pal@tcs.com>
2017-07-12 19:07:44 +0530

    Code Snippet

--- Events New/EventsWebService.swift
+++ Events New/EventsWebService.swift
@@ -9,5 +9,55 @@
 import UIKit
 
 class EventsWebService: NSObject {
-
+    
+    func fetchEvents(completion: @escaping (_ success: Bool, _ error: String?, _ events: [Event]?)->Void) {
+    
+        // Configuration
+        let session = URLSession(configuration: .default)
+        
+        var request = URLRequest(url: <#T##URL#>)
+        request.setValue(<#T##value: String?##String?#>, forHTTPHeaderField: <#T##String#>)
+        request.httpMethod = <#String GET or POST#>
+        request.timeoutInterval = <#Int#>
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            // Error
+            if let error = error {
+                DispatchQueue.main.async {
+                    // Completion
+                    <#code#>
+                }
+            }
+                
+                // Un Authorised
+            else if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                //Authorization Error
+                <#code#>
+            }
+                
+                // Proper response
+            else {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+                    {
+                        // Convert to Model Object
+                        <#code#>
+                    } else {
+                        // Parsing Error
+                        DispatchQueue.main.async {
+                            <#code#>
+                        }
+                    }
+                } catch {
+                    // Error parsing JSON
+                    DispatchQueue.main.async {
+                        <#code#>
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
 }
